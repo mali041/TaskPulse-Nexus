e0217618a2b89c57338a8c806b46d63a25fbc46b
mali041 <ali041gohar@gmail.com>
2024-05-29 10:10:54 +0500

    creating-user-profile-page

---
+++ client/src/pages/UpdateTask.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UpdateTask() {
+  return <div>UpdateTask</div>;
+}
+
+export default UpdateTask;
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -2,5 +2,18 @@ import Home from "./Home";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Profile from "./Profile";
+import CreateTask from "./CreateTask";
+import UpdateTask from "./UpdateTask";
+import SingleTask from "./SingleTask";
+import Tasks from "./Tasks";
 
-export { Home, SignIn, SignUp, Profile };
+export {
+  Home,
+  SignIn,
+  SignUp,
+  Profile,
+  CreateTask,
+  UpdateTask,
+  SingleTask,
+  Tasks,
+};
